844e35f9af4fa501c73fb2394ddb4cb5162eaece
ariannaarenee <arbryant@miners.utep.edu>
2024-09-30 00:35:16 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -4,15 +4,15 @@ big_mac_file = './big-mac-full-index.csv'
 
 def get_big_mac_price_by_year(year,country_code):
     pass # Remove this line and code your function
-
+$80_by_2015(2008,cad_cd)
 def get_big_mac_price_by_country(country_code):
     pass # Remove this line and code your function
-
+$40_by_phl(phl_php)
 def get_the_cheapest_big_mac_price_by_year(year):
     pass # Remove this line and code your function
-
+$50_by_2015(2013)
 def get_the_most_expensive_big_mac_price_by_year(year):
     pass # Remove this line and code your function
-
+$60_by_2022(2025)
 if __name__ == "__main__":
     pass # Remove this line and code your user interface
